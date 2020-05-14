3d0d13596397f022a26fae56f2e9a13bc9c9d02d
Mirko <mirkoletto@gmail.com>
2020-05-14 12:01:50 +0200

    Better documentation

--- src/client/connect4.h
+++ src/client/connect4.h
@@ -7,16 +7,23 @@
 
 
 class Connect4 {
+    /** Rows, cols and total size of the board */
     int rows_, cols_, size_;
+
+    /** Matrix data structure for the board */
     char* cells_;
+
+    /** Player marker */
     char player_;
+
+    /** Adversary marker */
     char adversary_;
 
     /** 
-     * Counts the tokens of the player towards a direction (di, dj)
+     * Counts the tokens of the player towards a direction (di, dj) 
      * starting from  (row, col)  
      * 
-     * @param player   player identifier for whom we count the tokens
+     * @param player   player marker for whom we count the tokens
      * @param row      starting row
      * @param col      starting col
      * @param di       step size for rows (each iteration row+di)
@@ -27,7 +34,20 @@ class Connect4 {
     int countNexts(char player, int row, int col, int di, int dj);
 
     public:
+
+    /**
+     * @brief Construct a new Connect 4 object
+     * 
+     * @param rows      Number of rows
+     * @param columns   Number of columns
+     */
     Connect4(int rows = 6, int columns = 7);
+
+    /**
+     * @brief Get the number of columns of the board
+     * 
+     * @return number of columns 
+     */
     int getNumCols();
 
     /**
@@ -36,19 +56,54 @@ class Connect4 {
      * @param column   target column where the token should be added
      * @param player   player who is making the move
      * 
-     * @return          0 on success without win
-     *                  1 on success with win
-     *                  -1 on failure for full column
-     *                  -2 on failure for full board
+     * @retval 1        Success with win
+     * @retval 0        Success without win
+     * @retval -1       Failure for full column
+     * @retval -2       Failure for full board
      */
     int8_t play(int column, char player = 0);
+
+    /**
+     * Checks if an inserted token causes a win
+     * 
+     * @param starting_row  row of the token
+     * @param starting_col  col of the token
+     * @param player        marker of the player inserting the token
+     *
+     * @return              true if winning, false otherwise
+     */
     bool checkWin(int starting_row, int starting_col, char player = 0);
 
+    /**
+     * @brief Sets the default player
+     * 
+     * @param player    player to be set
+     * @return true if a valid player was supplied and set
+     * @return false otherwise
+     */
     bool setPlayer(char player);
+
+    /**
+     * @brief Get the default player
+     * 
+     * @return player marker
+     */
     char getPlayer();
+
+    /**
+     * @brief Get the adversary, when a default player is set
+     * 
+     * @return enemy marker
+     */
     char getAdv();
 
+    /**
+     * @brief Prints the board
+     * 
+     * @param os    Output stream where the board has to be printed
+     */
     void print(std::ostream& os);
+
     friend std::ostream& operator<<(std::ostream& os, const Connect4& b);
 };
 #endif //CONNECT4_H
