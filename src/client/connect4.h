54dfec10fd3c35481951802d7f52e3cf43a7b23d
Mirko <mirkoletto@gmail.com>
2020-05-14 11:05:52 +0200

    Added doc

--- src/client/connect4.h
+++ src/client/connect4.h
@@ -12,15 +12,35 @@ class Connect4 {
     char player_;
     char adversary_;
 
-    /* 
+    /** 
      * Counts the tokens of the player towards a direction (di, dj)
      * starting from  (row, col)  
+     * 
+     * @param player   player identifier for whom we count the tokens
+     * @param row      starting row
+     * @param col      starting col
+     * @param di       step size for rows (each iteration row+di)
+     * @param dj       step size for cols (each iteration row+dj)
+     *
+     * @return         number of tokens along the (one way) direction
      */
     int countNexts(char player, int row, int col, int di, int dj);
 
     public:
     Connect4(int rows = 6, int columns = 7);
     int getNumCols();
+
+    /**
+     * Inserts a token.
+     *
+     * @param column   target column where the token should be added
+     * @param player   player who is making the move
+     * 
+     * @return          0 on success without win
+     *                  1 on success with win
+     *                  -1 on failure for full column
+     *                  -2 on failure for full board
+     */
     int8_t play(int column, char player = 0);
     bool checkWin(int starting_row, int starting_col, char player = 0);
 
