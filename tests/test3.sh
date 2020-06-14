a46cf795ae1fa20db15b6b17861a205400bf4f28
Riccardo Mancini <r.mancini@santannapisa.it>
2020-06-14 14:54:19 +0200

    Updated tests

--- tests/test3.sh
+++ tests/test3.sh
@@ -11,20 +11,24 @@ trap cleanup EXIT
 
 dir=$(dirname $0)
 
-timeout 7 "$dir/../dist/server/server" 11225 > "/tmp/server.out" &
+cd "$dir"
+
+timeout 10 ../dist/server/server 11225 ../certs/server_cert.pem ../certs/server_key.pem ../certs/ca_cert.pem ../certs/ca_crl.pem ../certs/ > /tmp/server.out &
 
 sleep 1
 
-sh "$dir/test3/movesCB.sh" | "$dir/../dist/client/client" > "/tmp/clientCB.out" &
+sh test3/movesCB.sh | ../dist/client/client ../certs/up_cert.pem ../certs/up_key.pem ../certs/ca_cert.pem ../certs/ca_crl.pem  > /tmp/clientCB.out &
 
 sleep 1
 
-sh "$dir/test3/movesCA.sh" | "$dir/../dist/client/client" > "/tmp/clientCA.out" &
+sh test3/movesCA.sh | ../dist/client/client ../certs/mirko_cert.pem ../certs/mirko_key.pem ../certs/ca_cert.pem ../certs/ca_crl.pem > /tmp/clientCA.out &
 
 wait
 
-outputA=$(grep -i "you lost"  "/tmp/clientCA.out" | wc -l)
-outputB=$(grep -i "you won" "/tmp/clientCB.out" | wc -l)
+outputA=$(grep -i "you lost" "/tmp/clientCA.out" | wc -l)
+outputB=$(grep -i "you won"  "/tmp/clientCB.out" | wc -l)
+
+cd -
 
 if [ "$outputA" -eq "1" ] && [ "$outputB" -eq "1" ]
 then  
